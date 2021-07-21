1d603f0550145d49a8bdcbc07025b48fd47a2d31
TakehiroKatayama <t.katayama.info@gmail.com>
2021-07-21 21:55:48 +0900

    usersモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
-  def disp_name
-    if age > 19
-      "#{name}さん"
-    elsif age > 10
-      "#{name}君"
-    elsif age > 0
-      "#{name}ちゃん"
-    else
-      '不正な数値です'
-    end
-  end
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :age, presence: true, numericality: true, inclusion: { in: 0..150 }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
 end
