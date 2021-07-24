5e3b0b23d324d4066b260af904b5e7f5a46fea4c
TakehiroKatayama <t.katayama.info@gmail.com>
2021-07-24 16:24:07 +0900

    indexのリクエストテストを追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   # ***** 以下を追加 *****
   describe 'GET #index' do
-    it 'リクエストが成功する' do
-      create_list(:user, 3)
-      get(users_path)
-      expect(response).to have_http_status(:ok)
+    subject { get(users_path) }
+    context 'ユーザーが存在するとき' do
+      before { create_list(:user, 3) }
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'name が表示されている' do
+        subject
+        expect(response.body).to include(*User.pluck(:name))
+      end
     end
   end
   # ***** 以上を追加 *****
