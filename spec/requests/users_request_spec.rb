473363fcbfbd1d936547c501879c9a0d408e2fc0
TakehiroKatayama <t.katayama.info@gmail.com>
2021-07-25 23:41:49 +0900

    showのリクエストテストを追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # ***** 以下を追加 *****
   describe 'GET #index' do
     subject { get(users_path) }
     context 'ユーザーが存在するとき' do
@@ -17,5 +16,40 @@ RSpec.describe 'Users', type: :request do
       end
     end
   end
-  # ***** 以上を追加 *****
+
+  describe 'GET #show' do
+    subject { get(user_path(user_id)) }
+    context 'ユーザーが存在するとき' do
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'name が表示されている' do
+        subject
+        expect(response.body).to include user.name
+      end
+
+      it 'age が表示されている' do
+        subject
+        expect(response.body).to include user.age.to_s
+      end
+
+      it 'email が表示されている' do
+        subject
+        expect(response.body).to include user.email
+      end
+    end
+
+    context ':idに対応するユーザーが存在しないとき' do
+      let(:user_id) { 1 }
+
+      it 'エラーが発生する' do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
